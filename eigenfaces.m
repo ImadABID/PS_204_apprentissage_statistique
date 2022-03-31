2465e20631069a906b9e6e76601e39d24296afa5
AHALLI Mohamed <Mohamed.Ahalli@bordeaux-inp.fr>
2022-04-01 00:50:14 +0200

    walo

--- eigenfaces.m
+++ eigenfaces.m
@@ -110,24 +110,100 @@ image_to_classify = double(image_to_classify(:));
 image_class = classify_k_NN(image_to_classify, data_trn, lb_trn, x_bar, U, l, N);
 fprintf("image class = %d\n", image_class);
 
-%% Matrice de confusion
-nbr_of_test_set = 6;
+% %% Matrice de confusion
+% nbr_of_test_set = 6;
+% 
+% confMat = zeros(Nc, Nc, nbr_of_test_set);
+% 
+% for test_set_index = 1:nbr_of_test_set
+%     folder_path = "./database/test"+test_set_index+"/";
+%     folder_path = folder_path{1}; % transforming from "string" to 'string'
+%     [data_test, lb_test_real, P, N_test, ~, ~] = data_extraction(folder_path);
+%     
+%     lb_test_predicted = zeros(N_test, 1);
+% 
+%     for image_index = 1:N_test
+%         lb_test_predicted(image_index) = classify_k_NN(data_test(:,image_index), data_trn, lb_trn, x_bar, U, l, N);
+%     end
+% 
+%     confMat(:,:, test_set_index) = confusionmat(lb_test_real, lb_test_predicted);
+% 
+%     confMat(:,:, test_set_index);
+% 
+% end
 
-confMat = zeros(Nc, Nc, nbr_of_test_set);
+%%
+image_visage1= data_trn(:,find(lb_trn==1));
+image_visage2= data_trn(:,find(lb_trn==5));
+image_visage3= data_trn(:,find(lb_trn==9));
 
-for test_set_index = 1:nbr_of_test_set
-    folder_path = "./database/test"+test_set_index+"/";
-    folder_path = folder_path{1}; % transforming from "string" to 'string'
-    [data_test, lb_test_real, P, N_test, ~, ~] = data_extraction(folder_path);
-    
-    lb_test_predicted = zeros(N_test, 1);
-
-    for image_index = 1:N_test
-        lb_test_predicted(image_index) = classify_k_NN(data_test(:,image_index), data_trn, lb_trn, x_bar, U, l, N);
-    end
-
-    confMat(:,:, test_set_index) = confusionmat(lb_test_real, lb_test_predicted);
-
-    confMat(:,:, test_set_index)
-
+[Z,W] = size(image_visage1);
+data_trn_2_w_1 = zeros(Z,W);
+data_trn_2_w_2 = zeros(Z,W);
+data_trn_2_w_3 = zeros(Z,W);
+for i = 1:W
+    data_trn_2_w_1(:, i) = x2w(image_visage1(:, i), x_bar, U, l);
+    data_trn_2_w_2(:, i) = x2w(image_visage2(:, i), x_bar, U, l);
+    data_trn_2_w_3(:, i) = x2w(image_visage3(:, i), x_bar, U, l);
 end
+
+
+figure,
+subplot(2,2,1)
+hold on
+scatter(data_trn_2_w_1(1,:),data_trn_2_w_1(2,:),'filled','red');
+scatter(mean(data_trn_2_w_1(1,:)),mean(data_trn_2_w_1(2,:)),'red');
+
+scatter(data_trn_2_w_2(1,:),data_trn_2_w_2(2,:),'filled','green');
+scatter(mean(data_trn_2_w_2(1,:)),mean(data_trn_2_w_2(2,:)),'green');
+
+scatter(data_trn_2_w_3(1,:),data_trn_2_w_3(2,:),'filled','blue');
+scatter(mean(data_trn_2_w_3(1,:)),mean(data_trn_2_w_3(2,:)),'blue');
+
+hold off
+legend('class 1','moyenne class 1','class 5','moyenne class 5','class 9','moyenne class 9');
+title('Couple (1,2)');
+  
+subplot(2,2,2)
+hold on
+scatter(data_trn_2_w_1(2,:),data_trn_2_w_1(3,:),'filled','red');
+scatter(mean(data_trn_2_w_1(2,:)),mean(data_trn_2_w_1(3,:)),'red');
+
+scatter(data_trn_2_w_2(2,:),data_trn_2_w_2(3,:),'filled','green');
+scatter(mean(data_trn_2_w_2(2,:)),mean(data_trn_2_w_2(3,:)),'green');
+
+scatter(data_trn_2_w_3(2,:),data_trn_2_w_3(3,:),'filled','blue');
+scatter(mean(data_trn_2_w_3(2,:)),mean(data_trn_2_w_3(3,:)),'blue');
+hold off
+legend('class 1','moyenne class 1','class 5','moyenne class 5','class 9','moyenne class 9');
+title('Couple (2,3)');
+
+subplot(2,2,3)
+hold on
+scatter(data_trn_2_w_1(3,:),data_trn_2_w_1(4,:),'filled','red');
+scatter(mean(data_trn_2_w_1(3,:)),mean(data_trn_2_w_1(4,:)),'red');
+
+scatter(data_trn_2_w_2(3,:),data_trn_2_w_2(4,:),'filled','green');
+scatter(mean(data_trn_2_w_2(3,:)),mean(data_trn_2_w_2(4,:)),'green');
+
+scatter(data_trn_2_w_3(3,:),data_trn_2_w_3(4,:),'filled','blue');
+scatter(mean(data_trn_2_w_3(3,:)),mean(data_trn_2_w_3(4,:)),'blue');
+hold off
+legend('class 1','moyenne class 1','class 5','moyenne class 5','class 9','moyenne class 9');
+title('Couple (3,4)');
+
+subplot(2,2,4)
+hold on
+
+scatter(data_trn_2_w_1(4,:),data_trn_2_w_1(5,:),'filled','red');
+scatter(mean(data_trn_2_w_1(4,:)),mean(data_trn_2_w_1(5,:)),'red');
+
+scatter(data_trn_2_w_2(4,:),data_trn_2_w_2(5,:),'filled','green');
+scatter(mean(data_trn_2_w_2(4,:)),mean(data_trn_2_w_2(5,:)),'green');
+
+scatter(data_trn_2_w_3(4,:),data_trn_2_w_3(5,:),'filled','blue');
+scatter(mean(data_trn_2_w_3(4,:)),mean(data_trn_2_w_3(5,:)),'blue');
+hold off
+
+legend('class 1','moyenne class 1','class 5','moyenne class 5','class 9','moyenne class 9');
+title('Couple (4,5)');
